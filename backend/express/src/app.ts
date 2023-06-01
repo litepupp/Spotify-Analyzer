6a87bea1ed5075d5d012dc7178202e7ed1aaca59
litepupp <116616252+litepupp@users.noreply.github.com>
2023-06-01 13:34:57 -0500

    removed migrations? good?

--- backend/express/src/app.ts
+++ backend/express/src/app.ts
@@ -1,10 +1,18 @@
+import { PrismaClient, Stream } from "@prisma/client";
 import express from "express";
 
 const app = express();
 const port = 3000;
 
+const prisma = new PrismaClient();
+
 app.get("/", (req, res) => {
-  res.send("OOF");
+  prisma.stream
+    .findMany()
+    .then((streams) => {
+      res.json(streams);
+    })
+    .finally(() => {});
 });
 
 app.listen(port, () => {
